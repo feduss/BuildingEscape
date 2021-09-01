3fd707fda205c7e5e870cc28c588913fa62d70ee
Federico Saccà <feduss96@gmail.com>
2021-09-01 12:59:45 +0200

    Aggiungere i file di progetto.

---
+++ Intermediate/Build/Win64/BuildingEscapeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30040
+#include "C:/Users/fedus/Documents/Progetti/Unreal Projects/BuildingEscape/Intermediate/Build/Win64/BuildingEscapeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
