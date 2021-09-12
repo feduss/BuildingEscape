acad28f19b2c9e4d02e430365397aa7874578dea
Federico Saccà <feduss96@gmail.com>
2021-09-12 20:08:14 +0200

    Remove other assets. Update gitignore. Add New "World Position" c++ Component and add new log following lecture 90.:Logging To The Output Log

---
+++ Source/BuildingEscape/WorldPosition.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldPosition.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Display, TEXT("Hello, i'm a gray log!"));
+	UE_LOG(LogTemp, Warning, TEXT("Hello, i'm a yellow log!"));
+	UE_LOG(LogTemp, Error, TEXT("Hello, i'm a red log!"));
+	
+}
+
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
