ba2b0fb027bc1aba62a986ee126b0bbaccba8687
Federico Saccà <feduss96@gmail.com>
2021-09-12 19:51:50 +0200

    Update gitignore. Delete some dirs on git

--- Intermediate/Build/Win64/BuildingEscapeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30040
-#include "C:/Users/fedus/Documents/Progetti/Unreal Projects/BuildingEscape/Intermediate/Build/Win64/BuildingEscapeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
